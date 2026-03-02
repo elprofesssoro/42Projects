9e8862d5c0dcb40ca81495944f2ba6bb21194316
Illia Dvinov <idvinov@c2r13s7.42wolfsburg.de>
2026-03-02 16:05:37 +0100

    Added first exercise

---
+++ Advanced/LinAlg/00/src.rs
@@ -0,0 +1,65 @@
+mod structs;
+
+use structs::{Vector, Matrix};
+
+
+
+fn main() {
+	let mut u = Vector::from([2., 3.]);
+	let v = Vector::from([5., 7.]);
+	u.add(&v);
+	u.print();
+	// [7.0]
+	// [10.0]
+	let mut u = Vector::from([2., 3.]);
+	let v = Vector::from([5., 7.]);
+	u.sub(&v);
+	u.print();
+	// [-3.0]
+	// [-4.0]
+	let mut u = Vector::from([2., 3.]);
+	u.scl(2.);
+	u.print();
+	// [4.0]
+	// [6.0]
+
+
+	let mut u = Matrix::from([
+	[1., 2.],
+	[3., 4.]
+	]);
+	let v = Matrix::from([
+	[7., 4.],
+	[-2., 2.]
+	]);
+	u.add(v);
+	17
+	Enter the Matrix An introduction to Linear Algebra
+	println!("{}", u);
+	// [8.0, 6.0]
+	// [1.0, 6.0]
+	let mut u = Matrix::from([
+	[1., 2.],
+	[3., 4.]
+	]);
+	let v = Matrix::from([
+	[7., 4.],
+	[-2., 2.]
+	]);
+	u.sub(v);
+	println!("{}", u);
+	// [-6.0, -2.0]
+	// [5.0, 2.0]
+	let mut u = Matrix::from([
+	[1., 2.],
+	[3., 4.]
+	]);
+	u.scl(2.);
+	println!("{}", u);
+	// [2.0, 4.0]
+	// [6.0, 8.0]
+}
+
+fn add(x: i32, y: i32) -> i32 {
+	x + y
+}
---
+++ Advanced/LinAlg/00/structs.rs
@@ -0,0 +1,95 @@
+use std::fmt::Display;
+use std::ops::{AddAssign, SubAssign, MulAssign};
+
+
+pub struct Vector<K> {
+    x: K,
+    y: K
+}
+
+impl<K> Vector<K> {
+    pub fn from(arr: [K; 2]) -> Self {
+        let [x, y] = arr;
+        Vector { x, y } 
+    }
+}
+
+impl<K: Display> Vector<K> {
+    pub fn print(&self) {
+        println!("({}, {})", self.x, self.y);
+    }
+} 
+
+impl<K: Copy> Vector<K> {
+    pub fn get(&self) -> (K, K) {
+        (self.x, self.y)
+    }
+}
+
+impl<K: Copy + AddAssign + SubAssign + MulAssign> Vector<K> {
+	pub fn add(&mut self, v: &Vector<K>) {
+		self.x += v.x;
+		self.y += v.y;
+	}
+
+	pub fn sub(&mut self, v: &Vector<K>) {
+		self.x -= v.x;
+		self.y -= v.y;
+	}
+
+	pub fn scl(&mut self, a: K) {
+		self.x *= a;
+		self.y *= a;
+	}
+}
+
+
+pub struct Matrix<K, const ROWS: usize, const COLS: usize> {
+    pub data: [[K; COLS]; ROWS],
+}
+
+impl<K, const ROWS: usize, const COLS: usize> Matrix<K, ROWS, COLS> {
+    pub fn from(data: [[K; COLS]; ROWS]) -> Self {
+        Matrix { data }
+    }
+
+    pub fn size(&self) -> (usize, usize) {
+        (ROWS, COLS)
+    }
+}
+
+impl<K: Display, const ROWS: usize, const COLS: usize> Matrix<K, ROWS, COLS> {
+    pub fn print(&self) {
+        for i in 0..ROWS {
+            for j in 0..COLS {
+                print!("{} ", self.data[i][j]);
+            }
+            println!();
+        }
+    }
+}
+
+impl<K: Copy + AddAssign + MulAssign + SubAssign, const ROWS: usize, const COLS: usize> Matrix<K, ROWS, COLS> {
+    pub fn add(&mut self, v: &Matrix<K, ROWS, COLS>) {
+        for i in 0..ROWS {
+            for j in 0..COLS {
+                self.data[i][j] += v.data[i][j];
+            }
+        }
+    }
+	fn sub(&mut self, v: &Matrix<K, ROWS, COLS>)
+	{
+        for i in 0..ROWS {
+            for j in 0..COLS {
+                self.data[i][j] -= v.data[i][j];
+            }
+        }
+	}
+	fn scl(&mut self, a: K) {
+		        for i in 0..ROWS {
+            for j in 0..COLS {
+                self.data[i][j] *= a;
+            }
+        }
+	}
+}
